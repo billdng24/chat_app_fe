20da35f5df1d581dbd36c56b2926d39cb5affb64
billdng24 <nguyenkudo8436@gmail.com>
2024-03-04 16:47:16 +0700

    fix domains

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://chat-app-be-a5ec4b2bfcd6.herokuapp.com/",
+  baseURL: "https://chat-app-be-a5ec4b2bfcd6.herokuapp.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
